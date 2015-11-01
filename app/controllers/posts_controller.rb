195b1cfd7241d4675604fb8c42d667e39445ba72
Steven Huang <szteven3@gmail.com>
2015-11-02 00:58:00 +0800

    let textarea change line working

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,17 +4,23 @@ class PostsController < ApplicationController
   before_filter :find_categories, only: [:index]
 
   def index
-    @posts = Post.all
+    @posts = Post.all.order(id: :desc)
   end
 
   def show
-
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.create(post_params)
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
   end
 
   def edit
